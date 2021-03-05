134600e5da524940ded95177ff9de90dba9b3828
Victor Stati <victorstti@outlook.com>
2021-03-05 13:58:39 -0300

    Inicial projeto Eventos

---
+++ ProjAgil.WebAPI/Data/DataContext.cs
@@ -0,0 +1,13 @@
+using Microsoft.EntityFrameworkCore;
+using ProjAgil.WebAPI.Model;
+
+namespace ProjAgil.WebAPI.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options){ }
+
+        public DbSet<Evento> Eventos { get; set; }
+        
+    }
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+## ProjAgil Eventos
